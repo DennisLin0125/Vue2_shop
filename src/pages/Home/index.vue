8cc495833e56b5e24862e34919b3786760165396
Dennis <740125aa@gmail.com>
2023-10-22 20:53:46 +0800

    Recommend組件完成

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -4,17 +4,21 @@
     <!-- 使用三級連動全局組件 -->
     <TypeNav />
     <ListContainer />
+    <Recommend />
   </div>
 </template>
 
 <script>
 // 引入其餘的組件
 import ListContainer from '@/pages/Home/ListContainer'
+import Recommend from '@/pages/Home/Recommend'
+
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Home",
   components: {
     ListContainer,
+    Recommend,
   }
 };
 </script>
