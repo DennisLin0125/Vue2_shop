47b7fd54fc662e4ded48123b6a879939817a0688
Dennis <740125aa@gmail.com>
2023-10-22 20:42:25 +0800

    ListContainer組件完成

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -3,13 +3,19 @@
   <div>
     <!-- 使用三級連動全局組件 -->
     <TypeNav />
+    <ListContainer />
   </div>
 </template>
 
 <script>
+// 引入其餘的組件
+import ListContainer from '@/pages/Home/ListContainer'
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Home",
+  components: {
+    ListContainer,
+  }
 };
 </script>
 
