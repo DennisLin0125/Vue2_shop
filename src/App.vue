41a731ddc060c23743b61f48b787b972019f536c
Dennis <740125aa@gmail.com>
2023-10-22 14:39:48 +0800

    Footter和Header組件完成

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    我是根組件
+    <Header />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
-
+  name: 'App',
+  components: { 
+    Header, 
+    Footer, 
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
